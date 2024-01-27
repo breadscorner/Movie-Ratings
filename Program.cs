4081212a66c2f70c60ab7858b1530f636d890eb2
Brett Gill <hello@brettgill.io>
2024-01-27 13:56:04 -0800

    add comments for reference

--- Program.cs
+++ Program.cs
@@ -4,15 +4,21 @@ using movies.Models;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+DotNetEnv.Env.Load();
+
+// Retrieve environment variables
 var PGHOST = Environment.GetEnvironmentVariable("PGHOST");
 var PGDATABASE = Environment.GetEnvironmentVariable("PGDATABASE");
 var PGUSER = Environment.GetEnvironmentVariable("PGUSER");
 var PGPASSWORD = Environment.GetEnvironmentVariable("PGPASSWORD");
 
+// Build connection string
 var connectionString = $"Host={PGHOST};Database={PGDATABASE};Username={PGUSER};Password={PGPASSWORD}";
 
+// Add services to the container.
 var services = new ServiceCollection();
 
+// Add database context
 services.AddDbContext<DatabaseContext>(
     opt =>
     {
