21874c86f70fb46936c091a9a3679f5a18937af4
Brett Gill <hello@brettgill.io>
2024-01-27 22:58:49 -0800

    code prettier
    Co-authored-by: Brett Gill <hello@brettgill.io>
    
    Co-authored-by: Stephane Lubendo <stephane.lubendo@gmail.com>

--- my-app-frontend/src/main.tsx
+++ my-app-frontend/src/main.tsx
@@ -1,7 +1,4 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
