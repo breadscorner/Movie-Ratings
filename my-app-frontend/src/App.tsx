b86b9c373189f997880d032717dd53849faa84e7
Brett Gill <hello@brettgill.io>
2024-01-27 16:54:52 -0800

    create movie and rating works

--- my-app-frontend/src/App.tsx
+++ my-app-frontend/src/App.tsx
@@ -4,32 +4,82 @@ import { useState, useEffect } from "react";
 type Movie = {
   id: number;
   title: string;
-  description: string;
+  year: number;
+  rating: number;
 };
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
- 
+  const [post, setPost] = useState(false);
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [year, setYear] = useState(0);
+
   useEffect(() => {
     async function fetchTodos() {
       const response = await fetch("/api/movies");
       const data = await response.json();
       setMovies(data);
-      console.log(data, 1);
     }
     fetchTodos();
     console.log("Hello");
-  }, []);
+  }, [post]);
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    setPost(true)
+    const response = await fetch("/api/Movies");
+    const data = await response.json();
+
+    const result = await fetch("/api/Movies", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: data.length + 1, title: title, rating: rating, year: year }),
+    })
+    const databody = result.json()
+    console.log(databody)
+    setPost(false);
+  }
 
   return (
     <>
-      <p>Hello</p>
+      <h1>Movie Ratings</h1>
+
+      {/* List all movies */}
       {movies.map((movie: Movie) => (
         <div key={movie.id}>
-          <p>{movie.title}</p>
-          <p>{movie.description}</p>
+          <h2>{movie.title}</h2>
+          <div>
+            <p>{movie.year}</p>
+            <p>Rating is {movie.rating} / 5</p>
+          </div>
         </div>
       ))}
+
+      {/* Add a movie */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="number"
+          name="rating"
+          placeholder="rating"
+          onChange={(e) => setRating(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          name="year"
+          placeholder="year"
+          onChange={(e) => setYear(parseInt(e.target.value))}
+        />
+        <button type="submit">Add Movie</button>
+      </form>
     </>
   );
 }
